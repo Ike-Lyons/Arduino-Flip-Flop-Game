d7704e7d9048e6f84f351dc3a65212d4d2982aa7
Ike-Lyons <spongelyons@gmail.com>
2016-01-06 10:56:05 -0500

    Create Flip-Flop.ino

---
+++ Flip-Flop.ino
@@ -0,0 +1,33 @@
+/* 
+Game using a 16x2 LCD and an arduino.The
+point of the game is to start with a
+random string of numbers containing one 
+of each number from 1-9. EX: 6847901532.
+The player then tries to order the string
+by fliping all the numbers behing the 
+cursoron the LCD. the win condition 
+is 0123456789.
+*/
+
+string winner = "0123456789"
+string changeingString = "0123456789"
+
+int rand;
+
+void start(){
+
+}
+
+void loop(){
+
+}
+
+void swaper(int a, int b){
+//swap numbers a and b in the changing string
+}
+
+void randomMaker(){
+//make the number random
+}
+
+
